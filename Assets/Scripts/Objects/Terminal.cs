84a8c004f470910e727644dad31b6eb1a8e11927
tim64 <zappazoid@gmail.com>
2018-07-01 12:01:55 +0700

    Добавлена мини игра про взлом терминала

---
+++ Assets/Scripts/Objects/Terminal.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terminal : MonoBehaviour {
+
+    public GameObject terminalWindow;
+    public TerminalHack hackText;
+    public GameObject infoText;
+    public Collider2D forceFieldCollider;
+    public ParticleSystem forceFieldParticle;
+
+    bool playerReady;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E) && playerReady)
+        {
+            terminalWindow.SetActive(true);
+            hackText.terminal = this;
+        }
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            playerReady = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            playerReady = false;
+        }
+    }
+
+    public void DeactivateField()
+    {
+        infoText.SetActive(false);
+        forceFieldCollider.enabled = false;
+        forceFieldParticle.Stop();
+    }
+}
---
+++ Assets/Scripts/UI/TerminalHack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TerminalHack : MonoBehaviour {
+
+    Text textfield;
+    public Terminal terminal;
+    public Image detector;
+    bool endupdate;
+    int charIndex = 0;
+    string codetext = "\n\n#include <stdio.h>\n\nmain()\n{\n\tprintf(hello, world!);\n}";
+
+    void Start () {
+        textfield = GetComponent<Text>();
+        //textfield.text = codetext;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!endupdate)
+        {
+            foreach (char c in Input.inputString)
+            {
+                if (charIndex < codetext.Length)
+                {
+                    textfield.text += codetext[charIndex];
+                    charIndex += 1;
+                }
+                else
+                {
+                    detector.color = Color.green;
+                    endupdate = true;
+                    textfield.text += "\nHack ready! Press enter!";
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && endupdate)
+        {
+            terminal.DeactivateField();
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
