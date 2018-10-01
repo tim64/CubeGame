f0058f600139911c878d256b4f6603ce29836dd5
tim64 <zappazoid@gmail.com>
2018-10-01 22:36:30 +0700

    Зона туториала. Новые чекпоинты

---
+++ Assets/Scripts/Objects/RespawnPillar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPillar : MonoBehaviour {
+
+	public GameObject souleye;
+	public GameObject lines;
+	public Sprite color_sprite;
+
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		print(other.tag);
+		if (other.tag == "Player") {
+			GetComponent<SpriteRenderer>().sprite = color_sprite;
+			souleye.GetComponent<SpriteRenderer>().color = Color.red;
+			lines.SetActive(true);
+		}
+	}
+
+
+}
