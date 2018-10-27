6be3507ae46003d937be647d06e69a7771c4618e
tim64 <zappazoid@gmail.com>
2018-10-27 12:18:36 +0700

    Первая кат-сцена
    Работа с таймлайном

---
+++ Assets/Scripts/UI/TerminalControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalControl : MonoBehaviour {
+
+	public Sprite terminalOff;
+	public Sprite terminalOn;
+	public bool state = false;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeState()
+	{
+		if (state == false)
+		{
+			GetComponent<SpriteRenderer>().sprite = terminalOn;
+			state = true;
+			return;
+		}
+		else
+		{
+			GetComponent<SpriteRenderer>().sprite = terminalOn;
+			state = false;
+			return;
+		}
+	}
+}
