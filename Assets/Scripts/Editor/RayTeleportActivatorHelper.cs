4f2f886c82efa544a07b3350202094fe029fcde7
Тимофей Нецветаев <timofei.netsvetaev@digital-ecosystems.ru>
2019-06-01 00:47:03 +0700

    Работа с активаторами телепорта (+OneLine)
    - Факелы теперь активируют главный телепорт
    - Подготовка к работе над событиями
    - Фиксы уровня
    - Добавление плагина OneLine

---
+++ Assets/Scripts/Editor/RayTeleportActivatorHelper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RayTeleportActivatorHelper : Editor
+{
+    SerializedProperty listProperty;
+
+    void OnEnable()
+    {
+        //listProperty = serializedObject.FindProperty("tourches");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //EditorGUI.PropertyField(new Rect(0, 300, 500, 30), listProperty, new GUIContent("List : "));
+
+       // serializedObject.ApplyModifiedProperties();
+    }
+}
